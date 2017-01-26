5314b1277261c20eacd5c8b22a5d9d2435d9904e
k3rm1t <K2600@student.jamk.fi>
2017-01-26 17:53:07 +0200

    Update Heater.cs
    i did it manually

--- heaterapplication/Heater.cs
+++ heaterapplication/Heater.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +9,34 @@ namespace Heaterapplication
     class Heater
     {
         // properties
+        public bool Power { get; set; }
+        public int Temp { get; set; }
+        public int Humid { get; set; }
+        public int Heating { get; set; }
+
         // constructors
-        // methods
+        // methods to change something
+        public Heater()
+        {
+
+        }
+        public void MoreTemp()
+        {
+            Temp += 10;
+        }
+        public void LessHeating()
+        {
+            Temp -= 10;
+        }
+
+        // methods to display properties
+        public void PrintData()
+        {
+            Console.WriteLine("Sauna is " + Power);
+            Console.WriteLine("There is " + Temp + " degrees");
+            Console.WriteLine("The humidity is " + Humid + " percent.");
+            
+        }
     }
+
 }
