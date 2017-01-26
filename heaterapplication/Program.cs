b198191a0cef221c2791032aed4535d9676054c7
k3rm1t <K2600@student.jamk.fi>
2017-01-26 17:53:51 +0200

    Update Program.cs
    manually added

--- heaterapplication/Program.cs
+++ heaterapplication/Program.cs
@@ -1,16 +1,29 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace heaterapplication
+namespace Heaterapplication
 {
     class Program
     {
         static void Main(string[] args)
         {
             // create one instance of Heater class
+            Heater heater = new Heater();
+            heater.PrintData();
+            heater.Power = true;
+            heater.Humid = 80;
+            heater.Temp = 90;
+            
+            heater.PrintData();
+            heater.MoreTemp();
+            heater.PrintData();
+            heater.LessHeating();
+            heater.LessHeating();
+            heater.PrintData();
+
         }
     }
 }
