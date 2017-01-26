10ba9aaedd7dc0ea22f2216218a9a1daa737de58
k2600 <k2600@OHJE-330-WS-32>
2017-01-26 10:24:06 +0200

    Add project files.

---
+++ Carapp/Car.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carapp
+{
+    class Car
+    {
+        // properties
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public double Engine { get; set; }
+        public int Speed { get; set; }
+        public bool FuzzyDices { get; set; }
+        public int DoorCount { get; set; }
+        public static int SomeProperty { get; set; }
+
+        //default constructor
+
+        public Car()
+        {
+
+        }
+
+        //constructor
+        public Car(string model)
+        {
+            Model = model;
+        }
+        
+        //method to give more speed
+        public void Accelerate()
+        {
+            Speed += 10;
+        }
+        //method slow down
+        public void Brake(int value)
+        {
+            Speed -= value;
+        }
+        //method to display car properties
+        public void PrintData()
+        {
+            Console.WriteLine("Car data: ");
+            Console.WriteLine("-model:" + Model);
+            Console.WriteLine("-color:" + Color);
+            Console.WriteLine("-engine:" + Engine);
+            Console.WriteLine("-speed:" + Speed);
+            Console.WriteLine("-fuzzydices:" + FuzzyDices);
+            Console.WriteLine("-doors:" + DoorCount);
+            Console.WriteLine("-else:" + SomeProperty);
+        }
+
+        //destructor
+        ~Car()
+        {
+            Console.WriteLine("Car object destroyed!");
+        }
+    }
+}
---
+++ Carapp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create one instance of car class
+            Car car = new Car();
+            car.PrintData();
+            car.Model = "Datasun 100A";
+            car.Color = "Grey";
+            car.Engine = 1.2;
+            car.Speed = 10;
+            car.FuzzyDices = true;
+            car.DoorCount = 2;
+            Car.SomeProperty = 100; //static property
+            car.PrintData();
+            car.Accelerate();
+            Console.WriteLine("Speed = " + car.Speed);
+            car.Brake(2);
+            Console.WriteLine("Speed = " + car.Speed);
+
+            //create a new instace from Car class
+            Car nascar = new Car("Speedstrep");
+            nascar.PrintData();
+            nascar.Color = "Black";
+            nascar.Engine = 6;
+            nascar.FuzzyDices = true;
+            nascar.PrintData();
+
+        }
+    }
+}
