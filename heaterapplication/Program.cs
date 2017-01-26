d9b848a75a8d9db8535f08454022a73c7351b2be
k2600 <k2600@student.jamk.fi>
2017-01-26 10:37:27 +0200

    Heater app added

---
+++ heaterapplication/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace heaterapplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create one instance of Heater class
+        }
+    }
+}
