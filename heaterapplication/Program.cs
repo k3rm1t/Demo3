1ea5da72f070290ddbbb19c34995f09bfb4698dc
Teemu Joutulainen <K2600@student.jamk.fi>
2017-01-28 15:07:53 +0200

    made on kala

--- heaterapplication/Program.cs
+++ heaterapplication/Program.cs
@@ -4,13 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace heaterapplication
+namespace Heaterapplication
 {
     class Program
     {
         static void Main(string[] args)
         {
             // create one instance of Heater class
+            Heater heater = new Heater();
+            heater.PrintData();
+            heater.Power = true;
+            heater.Humid = 80;
+            heater.Temp = 90;
+            heater.Brand = "Vuolux";
+
+            heater.PrintData();
+            heater.MoreHumid();
+            heater.MoreTemp();
+            heater.PrintData();
+            heater.LessTemp();
+            heater.LessTemp();
+            heater.LessHumid();
+            heater.PrintData();
+
         }
     }
 }
