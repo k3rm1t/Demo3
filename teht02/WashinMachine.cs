e53cdfc28a1cc9ee90040da2daba54ba648b7bd4
Teemu Joutulainen <K2600@student.jamk.fi>
2017-01-28 15:54:37 +0200

    made s

---
+++ teht02/WashinMachine.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teht02 //WashingMachineApplication
+{
+    class WashinMachine
+    {
+        //properties
+        public bool Power { get; set; }
+        public int Temp { get; set; }
+        public string Program { get; set; }
+        public int Spin { get; set; }
+        public string Brand { get; set;}
+        public int Delay { get; set; }
+        public bool Start { get; set; }
+        public bool Lock { get; set; }
+    //constructors
+    //methods to change something
+    //methods to display properties
+    public void PrintData()
+    {
+        Console.WriteLine("This washing machine is made by" + Brand );
+            if (Power == true)
+            {
+                Console.WriteLine("The machine is ON");
+            }
+            else
+            {
+                Console.WriteLine("Put the power ON!");
+            }
+            Console.WriteLine("Program is " + Program);
+            Console.WriteLine("Washing temperature is " + Temp + " degrees.");
+    }
+
+    }
+}
