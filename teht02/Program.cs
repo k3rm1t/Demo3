2e1ea71bc26f9c2c0771c36ca06d3dc8dcbf67c2
Teemu Joutulainen <K2600@student.jamk.fi>
2017-01-28 15:53:01 +0200

    made something

---
+++ teht02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teht02 //wahsing machine application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create one instance of WashincMachine class
+            WashinMachine machine = new WashinMachine();
+            machine.Brand = "ElectroLux";
+            machine.Power = true;
+            machine.Program = "Silk";
+            machine.Temp = 30;
+            machine.PrintData();
+        }
+    }
+}
