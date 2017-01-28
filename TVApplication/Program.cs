7e65dfa92c7a74739c1c3a695732daa3aa43666f
Teemu Joutulainen <K2600@student.jamk.fi>
2017-01-28 21:59:14 +0200

    jotaion

---
+++ TVApplication/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TVApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TV tv = new TV();
+            tv.Power = true;
+            tv.PowerOn();
+            tv.NextChannel();
+            tv.PrintData();
+        }
+    }
+}
