5203acb3b8ab42d96f6bb93c114032473e66e7e0
Teemu Joutulainen <K2600@student.jamk.fi>
2017-01-29 09:56:03 +0200

    more

--- TVApplication/TV.cs
+++ TVApplication/TV.cs
@@ -14,6 +14,7 @@ namespace TVApplication
         public bool Power { get; set; }
         public int Volume { get; set; }
         public int Channel { get; set; }
+        public string ChName { get; set; }
         public string Program { get; set; }
         public string Network { get; set; }
         public int brightness { get; set; }
@@ -29,25 +30,49 @@ namespace TVApplication
         public void NextChannel()
            
         {
-            while (Power == true)
+            if (Power == true)
             {
                 Channel += 1;
         } }
         public void PrevChannel()
         {
-            while (Power == true) { 
+            if (Power == true) { 
                 Channel -= 1;
         }
         }
         public void SelectChannel()
         {
             Console.WriteLine("Select channel you want to watch.");
+            Console.WriteLine("ÜLE 1\t1\nÜle 2\t2\nMainio TV\t3");
             int i = int.Parse(Console.ReadLine());
             Channel = i;
+        }
+        public void ShowProgram()
+        {
+            if (Channel == 1)
+            {
+                Program = "Üutiset";
+            }
+            else if (Channel == 2)
+            {
+                Program = "Die Kühe(Ratkaisijat)";
+            }
+            else Program = "Ostas TEEVee";
+            Console.WriteLine("Program you are watching is " + Program + " on channel " + Channel +" "+ ChName);
         }
         // Methods to display properties
         public void PrintData()
         {
+            switch (Channel) {
+                case 1: ChName = "ÜLE 1";
+                    break;
+                case 2: ChName = "ÜLE 2";
+                    break;
+                case 3: ChName= "Mainostus TeeVee 3";
+                    break;
+                default:Console.WriteLine("Chinese rise war");
+                    break;
+            }
             Console.WriteLine("Channel is " + Channel);
         }
         // Destructors
