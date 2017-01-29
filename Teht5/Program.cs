c4fd0f0ddf6ea31f2099a981eccf7a01ca9238b6
Teemu Joutulainen <K2600@student.jamk.fi>
2017-01-29 14:03:44 +0200

    doit

---
+++ Teht5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Teemu Joutulainen
+// Student Application practice
+// 29.01.2017
+namespace Teht5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.firstname = "Seppo";
+            student.lastname = "Maki";
+            student.PrintData();
+        }
+    }
+}
