3f3b16da408659c7e0719d01aa24c37960a2e2a4
Teemu Joutulainen <K2600@student.jamk.fi>
2017-01-29 14:03:59 +0200

    doit

---
+++ Teht5/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Teemu Joutulainen
+// Student Application practice
+// 29.01.2017
+
+namespace Teht5
+{
+    class Student
+    {
+        // fields 
+        public string firstname;
+        public string lastname;
+        // properties
+        public string Name
+        {
+            get { return firstname + " " + lastname; }
+        }
+        public string Address { get; set; }
+        public bool Presence { get; set; }
+        public string AddStudent { get; set; }
+        // default? constructor
+        // constructor
+        // methods to change something
+        // method to display something
+        
+        public void PrintData()
+        {
+            Console.WriteLine("Name: " + Name);
+        }
+        // destructor
+    }
+}
