22ff10e71c0936d258b4148e7344666f9c7edffb
Teemu Joutulainen <K2600@student.jamk.fi>
2017-01-29 13:34:23 +0200

    joo

---
+++ VechileApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Teemu Joutulainen
+// Vechile Application practice
+// 29.01.2017
+namespace VechileApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vechile vechile = new Vechile();
+            vechile.Name = "Audi";
+            vechile.Model = "A4";
+            vechile.Speed = 120;
+            vechile.Tyres = 20;
+            vechile.PrintData();
+        }
+    }
+}
