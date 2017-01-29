98b82cdfc8e07276994163801150de2d6dc52ed9
Teemu Joutulainen <K2600@student.jamk.fi>
2017-01-29 15:31:04 +0200

    ku

--- Teht5/Student.cs
+++ Teht5/Student.cs
@@ -12,6 +12,7 @@ namespace Teht5
     class Student
     {
         // fields 
+        public string[] strings = new string[5];
         public string firstname;
         public string lastname;
         // properties
@@ -20,6 +21,7 @@ namespace Teht5
             get { return firstname + " " + lastname; }
         }
         public string Address { get; set; }
+        public string StudentId { get; set; }
         public bool Presence { get; set; }
         public string AddStudent { get; set; }
         // default? constructor
@@ -27,10 +29,30 @@ namespace Teht5
         // methods to change something
         // method to display something
         
+        public string this[int i]
+        {
+            get
+            {
+                return strings[i];
+            }
+            set
+            {
+                strings[i] = Name +"\n"+ Address + "\n" + StudentId;
+            }
+            //Data[0] =  firstname +"\n" + lastname + "\n" + StudentId + "\n" + Address;
+        }
         public void PrintData()
         {
             Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Student Id: " + StudentId);
+            Console.WriteLine("Precense: " + Presence);
+            
+            //Console.WriteLine("Info: " + Data[0]);
         }
         // destructor
+        ~Student()
+        {
+            Console.WriteLine("Object destroyed.");
+        }
     }
 }
